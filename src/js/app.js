1e7a0856b0badf631825aa26d756d42f5fa00262
willconstable <will_constable@msn.com>
2020-06-17 06:59:44 +0100

    Update mobile navbar

--- src/js/app.js
+++ src/js/app.js
@@ -12,11 +12,15 @@ hamburgerBtn.addEventListener("click", e => {
     if(linkStatus === 0) {
         links.style.overflow = "visible";
         links.style.opacity = "1";
-        navbar.style.height = "35rem";
+        navbar.style.height = "100vh";
+        linkItem.style.height = "100vh";
+        linkItem.style.width = "100vw";
         linkStatus = 1;
     } else if(linkStatus === 1) {
         navbar.style.height = "10rem";
         links.style.opacity = "0";
+        linkItem.style.height = "auto";
+        linkItem.style.width = "auto";
         links.style.overflow = "hidden";
         linkStatus = 0;
     }
@@ -27,7 +31,10 @@ window.addEventListener("resize", e => {
     if(window.innerWidth > 800) {
         links.style.opacity = "1";
         navbar.style.height = "10rem";
+        links.style.overflow = "hidden";
         hamburgerBtn.classList.remove("active");
+        linkItem.style.height = "auto";
+        linkItem.style.width = "auto";
         linkStatus = 0;
     }
 });
